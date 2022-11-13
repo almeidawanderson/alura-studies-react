6a7a57eca228ffe3e643897feb219f08f21bc7fb
almeidawanderson <wandersonalmeida.eng@gmail.com>
2022-11-13 13:31:41 -0300

    create-component-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import React from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Formulario />
     </div>
   );
 }
