9705e6670513543684c10c2c0a71e93cdbc16c24
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-04-11 11:10:31 -0300

    feat: adjustment and implment css-modules in components

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Botao from "../botton";
-import './style.scss'
+import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
     render() {
         return(
-            <form className="novaTarefa">
-                <div className="inputContainer">
+            <form className={style.novaTarefa}>
+                <div className={style.inputContainer}>
                     <label htmlFor="Tarefa">
                         Adicione um novo estudo
                     </label>
@@ -20,7 +20,7 @@ class Formulario extends React.Component {
                     />
 
                 </div>
-                <div className="inputContainer">
+                <div className={style.inputContainer}>
                     <label htmlFor="Tempo">
                         Tempo
                     </label>
