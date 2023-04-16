25e1e8e01be04b9d9dc6b0d414d191d207957887
Wanderson Almeida <wandersonalmeida.eng@gmail.com>
2023-04-16 14:53:49 -0300

    feat: implement logic in item.tsx

--- src/components/Lista/item/index.tsx
+++ src/components/Lista/item/index.tsx
@@ -2,6 +2,8 @@ import { ITarefa } from '../../../types/ITarefa';
 import style from '../Lista.module.scss';
 
 export default function Item({tarefa, tempo, selecionado, completado, id}: ITarefa) {
+  console.log('item atual:', {tarefa, tempo, selecionado, completado, id});
+  
   
   return (
     <li className={style.item}>
